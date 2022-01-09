414105b3b49b9d28162d5172728a9c00570e182a
ManishGotame <manishgt194@gmail.com>
2022-01-09 18:00:08 +0545

    test firebase again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello world What </h1>
+        <h1> Hello world What the hell</h1>
         <h2> {this.state.value} </h2>
       </div>
     );
