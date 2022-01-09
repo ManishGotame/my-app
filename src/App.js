e2bdd4db51ef24b84caa96b3edf757f1139531c8
ManishGotame <manishgt194@gmail.com>
2022-01-09 16:46:24 +0545

    init files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from "react";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello world </h1>
+        <h2> {this.state.value} </h2>
+      </div>
+    );
+  }
+}
+
+
+export default App;
