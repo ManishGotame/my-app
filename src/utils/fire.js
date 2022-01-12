d001b057fb6f04e1c0e9b9039ff8c9d5eebafc45
ManishGotame <manishgt194@gmail.com>
2022-01-12 19:33:18 +0545

    pipeline deploy

--- src/utils/fire.js
+++ src/utils/fire.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/compat/app';
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import 'firebase/auth';
 
 const firebaseConfig = {
